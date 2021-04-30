5147defe29e506a8a6c3af6a20d6c1e24d20f782
Conna Wiles <kurozael@gmail.com>
2021-04-30 14:14:34 +0100

    Added TriggerWhiteArea for where the white ball can be placed (stub)

---
+++ code/entities/TriggerWhiteArea.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolGame
+{
+	[Library( "trigger_white_area" )]
+	public partial class TriggerWhiteArea : BaseTrigger
+	{
+		public override void StartTouch( Entity other )
+		{
+			base.StartTouch( other );
+		}
+	}
+}
