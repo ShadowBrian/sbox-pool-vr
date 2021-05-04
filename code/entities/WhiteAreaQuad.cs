a7b77403d163c7414efa6be86f539601ba56e323
Garry Newman <garrynewman@users.noreply.github.com>
2021-05-04 17:07:31 +0100

    Compile FIx

--- code/entities/WhiteAreaQuad.cs
+++ code/entities/WhiteAreaQuad.cs
@@ -11,26 +11,21 @@ namespace PoolGame
 	public partial class WhiteAreaQuad : RenderEntity
 	{
 		[Net] public BBox Bounds { get; set; }
-		public Material Material { get; set; }
+		public Material Material = Material.Load( "materials/dev/dev_measuregeneric01.vmat" );
 
 		public override void Spawn()
 		{
 			base.Spawn();
 		}
 
-		public override void DoRender()
+		public override void DoRender( SceneObject sceneObject  )
 		{
-			if ( Material == null )
-				Material = Material.Load( "materials/dev/dev_measuregeneric01.vmat" );
-
 			var rect = new Rect( Bounds.Mins.x, Bounds.Mins.y, Bounds.Maxs.x - Bounds.Mins.x, Bounds.Maxs.y - Bounds.Mins.y );
 
 			Render.Color = Color.White;
 			Render.Material = Material;
 			Render.CullMode = CullMode.None;
 			Render.DrawQuad( rect );
-
-			base.DoRender();
 		}
 	}
 }
