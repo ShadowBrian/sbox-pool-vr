093c07b069bf465d0022b8c5475875ae807e84c9
Lewis Ainslie <hello@lewwis.com>
2021-05-13 15:33:30 +0100

    Add re-designed header

--- code/ui/PlayerDisplay.cs
+++ code/ui/PlayerDisplay.cs
@@ -13,15 +13,7 @@ namespace PoolGame
 		public Image Avatar;
 
 		public Panel ScoreContainer;
-		public Label Score;
 		public Panel BallType;
-
-		public Panel TimeBarWrapper;
-		public Panel TimeRemainingBar;
-		public Panel TimeRemainingProgress;
-
-		public Panel TimeRemainingNumber;
-		public Label TimeRemainingLabel;
 
 		public PlayerDisplayItem()
 		{
@@ -30,17 +22,7 @@ namespace PoolGame
 			Name = PlayerContainer.Add.Label("Name", "name");
 
 			ScoreContainer = Add.Panel("score-container");
-			Score = ScoreContainer.Add.Label("0", "score");
 			BallType = ScoreContainer.Add.Panel("ball");
-
-
-			TimeBarWrapper = Add.Panel( "time-bar-wrapper" );
-
-			TimeRemainingBar = TimeBarWrapper.Add.Panel( "time-remaining" );
-			TimeRemainingProgress = TimeRemainingBar.Add.Panel( "time-remaining-progress" );
-
-			TimeRemainingNumber = TimeBarWrapper.Add.Panel( "time-remaining-number" );
-			TimeRemainingLabel = TimeRemainingNumber.Add.Label( "30", "time-remaining-label" );
 
 		}
 
@@ -56,30 +38,13 @@ namespace PoolGame
 			if ( isValid )
 			{
 				Name.Text = player.Entity.Name;
-				Score.Text = player.Entity.Score.ToString();
 
 				Avatar.SetTexture( $"avatar:{player.Entity.SteamId}" );
 
 				BallType.SetClass( "spots", player.Entity.BallType == PoolBallType.Spots );
 				BallType.SetClass( "stripes", player.Entity.BallType == PoolBallType.Stripes );
 
-				SetClass( "active", player.Entity.IsTurn );
-
-
-				TimeRemainingProgress.Style.Width = Length.Percent( (100f / 30f) * round.TimeLeftSeconds );
-				TimeRemainingProgress.Style.Dirty();
-
-				if ( round.TimeLeftSeconds <= 5 )
-				{
-					TimeBarWrapper.SetClass( "low", true );
-				}
-				else
-				{
-					TimeBarWrapper.SetClass( "low", false );
-				}
-
-				TimeRemainingLabel.Text = round.TimeLeftSeconds.ToString();
-
+				SetClass( "active", player.Entity.IsTurn );	
 			}
 
 			SetClass( "hidden", !isValid );
@@ -91,11 +56,26 @@ namespace PoolGame
 		public PlayerDisplayItem PlayerOne;
 		public PlayerDisplayItem PlayerTwo;
 
+		public Panel TimeRemainingNumber;
+		public Label TimeRemainingLabel;
+
+		public Panel TimeBarWrapper;
+		public Panel TimeRemainingBar;
+		public Panel TimeRemainingProgress;
+
 		public PlayerDisplay()
 		{
 			StyleSheet.Load( "/ui/PlayerDisplay.scss" );
 
 			PlayerOne = AddChild<PlayerDisplayItem>( "one" );
+
+			TimeRemainingNumber = Add.Panel( "time-remaining-number" );
+			TimeRemainingLabel = TimeRemainingNumber.Add.Label( "30", "time-remaining-label" );
+
+			TimeBarWrapper = Add.Panel( "time-bar-wrapper" );
+			TimeRemainingBar = TimeBarWrapper.Add.Panel( "time-remaining" );
+			TimeRemainingProgress = TimeRemainingBar.Add.Panel( "time-remaining-progress" );
+
 			PlayerTwo = AddChild<PlayerDisplayItem>( "two" );
 		}
 
@@ -109,6 +89,7 @@ namespace PoolGame
 
 			var round = game.Round;
 
+
 			if ( round is not PlayRound )
 			{
 				SetClass( "hidden", true );
@@ -117,6 +98,23 @@ namespace PoolGame
 
 			PlayerOne.Update( Game.Instance.PlayerOne );
 			PlayerTwo.Update( Game.Instance.PlayerTwo );
+
+
+			TimeRemainingLabel.Text = round.TimeLeftSeconds.ToString();
+
+			TimeRemainingProgress.Style.Width = Length.Percent( (100f / 30f) * round.TimeLeftSeconds );
+			TimeRemainingProgress.Style.Dirty();
+
+			if ( round.TimeLeftSeconds <= 5 )
+			{
+				TimeBarWrapper.SetClass( "low", true );
+				TimeRemainingNumber.SetClass( "low", true );
+			}
+			else
+			{
+				TimeBarWrapper.SetClass( "low", false );
+				TimeRemainingNumber.SetClass( "low", false );
+			}
 
 			SetClass( "hidden", false );
 		}
